f0bb247c02fb52b3ca38dbbd54adb909f1fe44a3
lurkaround <rhelon@gmail.com>
2021-05-23 13:46:24 +0200

    Completed signin/signout/signup flow

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+const usersRepo = require('./repositories/users');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    keys: ['lkacnlkei3023slsk4l4'],
+  })
+);
 
-app.get('/', (req, res) => {
+app.get('/signup', (req, res) => {
   res.send(
     `
   <div>
@@ -38,9 +45,61 @@ app.get('/', (req, res) => {
 //   }
 // };
 
-app.post('/', (req, res) => {
-  console.log(req.body);
+app.post('/signup', async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body;
+  const existingUser = await usersRepo.getOneBy({ email });
+  if (existingUser) {
+    return res.send('Account already exists');
+  }
+
+  if (password !== passwordConfirmation) {
+    return res.send('Passwords must match');
+  }
+
+  const user = await usersRepo.create({ email, password });
+
+  req.session.userId = user.id;
+
   res.send('Account successfully created.');
+});
+
+app.get('/signout', (req, res) => {
+  req.session = null;
+  res.send('You are logged out');
+});
+
+app.get('/signin', (req, res) => {
+  res.send(`
+  <div>
+    <form method="POST" action=''>
+        <input name="email" type"text" placeholder="email" />
+        <input name="password" type="password" placeholder="password" />
+        <button>Sign In</button>
+    </form>
+
+  </div>
+  `);
+});
+
+app.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+  const user = await usersRepo.getOneBy({ email });
+  if (!user) {
+    res.send('Email not found');
+  }
+
+  const validPassword = await usersRepo.comparePasswords(
+    user.password,
+    password
+  );
+
+  if (!validPassword) {
+    return res.send('Invalid Password');
+  }
+
+  req.session.userId = user.id;
+
+  res.send('You are signed in.');
 });
 
 app.listen(3000, () => {
--- repositories/users.js
+++ repositories/users.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const crypto = require('crypto');
+const util = require('util');
+
+const scrypt = util.promisify(crypto.scrypt);
 
 class UsersRepository {
   constructor(filename) {
@@ -25,9 +28,29 @@ class UsersRepository {
 
   async create(attrs) {
     attrs.id = this.randomId();
+    const salt = crypto.randomBytes(8).toString('hex');
+    const buf = await scrypt(attrs.password, salt, 64);
+
     const records = await this.getAll();
-    records.push(attrs);
+    const record = {
+      ...attrs,
+      password: `${buf.toString('hex')}.${salt}`,
+    };
+
+    records.push(record);
     await this.writeAll(records);
+    return record;
+  }
+
+  async comparePasswords(saved, supplied) {
+    // saved => password saved in database -> hased.salt
+    // supplied => pw given by user signing in
+    // const result = saved.split('.');
+    // const hashed = result[0];
+    // const salt = result[1];
+    const [hashed, salt] = saved.split('.');
+    const hashedSuppliedBuf = await scrypt(supplied, salt, 64);
+    return hashed === hashedSuppliedBuf.toString('hex');
   }
 
   async writeAll(records) {
@@ -80,16 +103,18 @@ class UsersRepository {
   }
 }
 
-const test = async () => {
-  const repo = new UsersRepository('users.json');
-  // await repo.create({ email: 'test@test.com', password: 'password' });
-  // const users = await repo.getAll();
-  // const user = await repo.getOne('6096e892');
-  // console.log(user);
-  // await repo.delete('6096e892');
-  // await repo.update('7186e892', { password: 'mypassword' });
-  const user = await repo.getOneBy({ email: 'test@test.com' });
-  console.log(user);
-};
+// const test = async () => {
+//   const repo = new UsersRepository('users.json');
+//   // await repo.create({ email: 'test@test.com', password: 'password' });
+//   // const users = await repo.getAll();
+//   // const user = await repo.getOne('6096e892');
+//   // console.log(user);
+//   // await repo.delete('6096e892');
+//   // await repo.update('7186e892', { password: 'mypassword' });
+//   const user = await repo.getOneBy({ email: 'test@test.com' });
+//   console.log(user);
+// };
 
-test();
+// test();
+
+module.exports = new UsersRepository('users.json');
