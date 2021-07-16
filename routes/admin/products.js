687135122fab6205cb86011a287d3f2a98dc8ce2
lurkaround <rhelon@gmail.com>
2021-07-16 10:15:16 +0200

    fixed handleErrors middleware

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -47,13 +47,26 @@ router.get('/admin/products/:id/edit', requireAuth, async (req, res) => {
 });
 
 router.post(
-  'admin/products/:id/edit',
-
+  '/admin/products/:id/edit',
   requireAuth,
   upload.single('image'),
   [requireTitle, requirePrice],
-  handleErrors(productsEditTemplate),
-  async (req, res) => {}
+  handleErrors(productsEditTemplate, async (req) => {
+    const product = await productsRepo.getOne(req.params.id);
+    return { product };
+  }),
+  async (req, res) => {
+    const changes = req.body;
+    if (req.file) {
+      changes.image = req.file.buffer.toString('base64');
+    }
+    try {
+      await productsRepo.update(req.params.id, changes);
+    } catch (err) {
+      return res.send('could not find item');
+    }
+    res.redirect('/admin/products');
+  }
 );
 
 module.exports = router;
