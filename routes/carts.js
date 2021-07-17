bc0e58eb42940844bf15dc07ca2bda064abcde72
lurkaround <rhelon@gmail.com>
2021-07-17 11:22:15 +0200

    Added base shopping cart route

---
+++ routes/carts.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+// receive a post request to add an item to cart
+
+// receive a GET request so show all items in cart
+
+// receive a post request to delete an item from a cart
+
+module.exports = router;
