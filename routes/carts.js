f406b8da1325f2c289a54bdeb75600cc65fff10f
lurkaround <rhelon@gmail.com>
2021-07-17 15:34:16 +0200

    Added product edit redirects and delete functionality

--- routes/carts.js
+++ routes/carts.js
@@ -35,7 +35,7 @@ router.post('/cart/products', async (req, res) => {
     items: cart.items,
   });
 
-  res.send('product added to cart');
+  res.redirect('/cart');
 });
 // OR add new product to items array
 // receive a GET request to show all items in cart
@@ -55,5 +55,14 @@ router.get('/cart', async (req, res) => {
 });
 
 // receive a post request to delete an item from a cart
+router.post('/cart/products/delete', async (req, res) => {
+  const { itemId } = req.body;
+  const cart = await cartsRepo.getOne(req.session.cartId);
+
+  const items = cart.items.filter((item) => item.id !== itemId);
+  await cartsRepo.update(req.session.cartId, { items });
+  console.log(cart);
+  res.redirect('/cart');
+});
 
 module.exports = router;
